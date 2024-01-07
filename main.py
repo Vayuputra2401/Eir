1abc562249efe50e05aab66bfad63d684031e375
Vayuputra2401 <pathikreetofficial@gmail.com>
2024-01-07 23:57:20 +0530

    Traning complete

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
-STAGE_NAME = "Data Ingestion stage"
+'''STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
@@ -30,7 +30,7 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
-        raise e
+        raise e'''
 
 
 
@@ -48,7 +48,7 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Evaluation stage"
+'''STAGE_NAME = "Evaluation stage"
 try:
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -59,5 +59,5 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
+'''
 
